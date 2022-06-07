127f0a5c7f28a2754aa4432906362e999cce989b
yodgor777 <kyodgorbek@gmail.com>
2022-06-07 21:13:05 +0200

    Removed words from MainActivity.kt

--- app/src/main/java/com/example/stockmarket/MainActivity.kt
+++ app/src/main/java/com/example/stockmarket/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StockMarketTheme {
-                // A surface container using the 'background' color from the theme
+                //
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
